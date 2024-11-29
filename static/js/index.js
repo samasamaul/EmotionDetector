e9810b79fa6ba73b544d1877ba425a5084edd3b7
samasamaul <ancritbetbeliauayonima@gmail.com>
2024-11-29 09:20:32 +0700

    Fix MultiFace

--- static/js/index.js
+++ static/js/index.js
@@ -54,7 +54,7 @@ video.addEventListener('play', () => {
 
             faceapi.draw.drawDetections(canvas, resizedDetections);
             faceapi.draw.drawFaceLandmarks(canvas, resizedDetections);
-            faceapi.draw.drawFaceExpressions(canvas, resizedDetections);
+            // faceapi.draw.drawFaceExpressions(canvas, resizedDetections);
 
             resizedDetections.forEach(detection => {
                 const box = detection.detection.box;
